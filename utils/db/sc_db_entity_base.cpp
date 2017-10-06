9f33f3c64829d7245f522c9f811cf40965b7e15e
Timoshenko <softctrl@users.noreply.github.com>
2017-10-06 08:16:34 -0300

    Create sc_db_entity_base.cpp

---
+++ utils/db/sc_db_entity_base.cpp
@@ -0,0 +1,32 @@
+/*
+ *
+ * Author:
+ *  Carlos Timoshenko
+ *  carlostimoshenkorodrigueslopes@gmail.com
+ *
+ *  https://github.com/softctrl
+ *
+ * This project is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+#include "sc_db_entity_base.hpp"
+
+const char* SCDBEntityBase::F_ID = "_id";
+
+/**
+ * @brief SCDBEntityBase::SCDBEntityBase
+ */
+SCDBEntityBase::SCDBEntityBase() {}
+
+/**
+ * @brief SCDBEntityBase::~SCDBEntityBase
+ */
+SCDBEntityBase::~SCDBEntityBase() {}
